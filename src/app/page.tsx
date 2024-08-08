e82ceb8855c6a75e699376307e03f6e1858122ee
hisham <dulfackerhisham@gamil>
2024-08-08 19:16:20 +0530

    dynamically routed when user input texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 'use client'
 
 import { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const [inputVal, setInputVal] = useState("");
 
+  const {push} = useRouter();
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    
+    push(`/prediction/${inputVal}`)
+
   };
 
   return (
@@ -15,8 +19,9 @@ export default function Home() {
     <div>
       <h1>enter your name</h1>
     </div>
-    <form action="">
-      <input type="text" placeholder="type your name.." onChange={(e) => setInputVal(e.target.value)} />
+    <form onSubmit={handleSubmit}>
+      <input type="text"  className="text-black" value={inputVal}
+      placeholder="type your name.." onChange={(e) => setInputVal(e.target.value)} />
       <button type="submit" >Predic Data</button>
     </form>
    </div>
